9de02d7648a2304bb7f97b53765b61f4741d7ba9
Gfarf <gfpalu@gmail.com>
2025-09-30 11:50:12 -0300

    listener active

---
+++ get_lines_channel.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	messages := make(chan string)
+	go func() {
+		defer close(messages)
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					messages <- currentLineContents
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				messages <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+	return messages
+}
--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	fmt.Println("===== Loop will start ======")
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("===== Connection Accepted ======")
+		lineChannels := getLinesChannel(connection)
+		for line := range lineChannels {
+			fmt.Println(line)
+		}
+		fmt.Println("===== Connection And Channel Closed ======")
+	}
+
 }
